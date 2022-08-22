8fe5bd9d1ef0c43d971ecdaea8876873dc3a5f49
khdkkhdd <khdkkhdd@gmail.com>
2022-08-22 11:00:39 +0900

    Initial Commit

---
+++ spring-basic/core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo") // localhost:8080/log-demo 로 접속을 하면 아래 메서드를 실행해라.
+    @ResponseBody
+    // HttpServletRequest는 서버가 생성하는 객체로,  request에 대한 정보들을 전달하게 된다.
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
