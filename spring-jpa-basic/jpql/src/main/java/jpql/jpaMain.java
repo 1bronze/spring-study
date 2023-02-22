3f84165013c5c7f716848b91ef23823a7a4d872d
1Bronze <khdkkhdd@gmail.com>
2023-02-23 00:57:53 +0900

    JPA 기본편 - JPQL 프로젝트 생성

--- spring-jpa-basic/jpql/src/main/java/jpql/jpaMain.java
+++ spring-jpa-basic/jpql/src/main/java/jpql/jpaMain.java
@@ -1,13 +1,13 @@
-package hellojpa;
+package jpql;
 
-import org.hibernate.Hibernate;
-
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
-import java.util.Set;
 
 public class jpaMain {
 
@@ -20,13 +20,9 @@ public class jpaMain {
 
         try {
 
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member> query = cb.createQuery(Member.class);
-
-            Root<Member> m = query.from(Member.class);
-
-            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
-            List<Member> resultList = em.createQuery(cq).getResultList();
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
