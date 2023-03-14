8d4f877e7b06edc52dc8fd04ade3408f56659566
1Bronze <khdkkhdd@gmail.com>
2023-03-14 17:48:20 +0900

    스프링 부트와 aws로 혼자 구현하는 웹 서비스 - 3장

---
+++ spring-aws/book/src/main/java/com/springaws/book/service/posts/PostsService.java
@@ -0,0 +1,34 @@
+package com.springaws.book.service.posts;
+
+import com.springaws.book.domain.posts.Posts;
+import com.springaws.book.domain.posts.PostsRepository;
+import com.springaws.book.web.dto.PostsResponseDto;
+import com.springaws.book.web.dto.PostsSaveRequestDto;
+import com.springaws.book.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
