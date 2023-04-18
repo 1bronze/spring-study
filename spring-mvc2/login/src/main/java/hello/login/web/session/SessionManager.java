cdf3e3e9e51fc75b8f3db1dfd8b08f735ac12cf2
1Bronze <khdkkhdd@gmail.com>
2023-04-18 18:21:14 +0900

    MVC 2편 - 로그인 처리1 (쿠키, 세션)

---
+++ spring-mvc2/login/src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,53 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class SessionManager {
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    public void createSession(Object value, HttpServletResponse response) {
+
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(mySessionCookie);
+    }
+
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if(sessionCookie==null) {
+            return null;
+        }
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if(sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
+        if(request.getCookies() == null) {
+            return null;
+        }
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+    }
+}
